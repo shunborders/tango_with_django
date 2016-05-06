5545c13fa752c38107d1e05c8a1218b9baf34733
shunborders <borders.shun@gmail.com>
2016-05-06 11:32:45 -0400

    Added files via upload

---
+++ tango_with_django/urls.py
@@ -0,0 +1,34 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+from registration.backends.simple.views import RegistrationView
+from django.shortcuts import render
+
+
+
+# Create a new class that redirects the user to the index page, if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/'
+        #return '/rango/profile'
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^rango/', include('rango.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+
+)
+
+
+if settings.DEBUG:
+    urlpatterns +=patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+         'serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
